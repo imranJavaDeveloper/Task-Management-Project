240aedc25edd41daede2c3c9c411d9d7bbf7ca0b
Md Imran Alam <mdimranalam2348@gmail.com>
2024-11-27 09:37:13 +0530

    remove some file

--- Task-Manager/src/main/java/in/codegram/Task/Manager/repositroy/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-
-import in.codegram.Task.Manager.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task,Long> {
-}
